d66eeaf92009707e162614ef62f6ddacf6cbb31c
matthias-codes <matthias00187@gmail.com>
2023-03-26 18:44:07 +0200

    Add export "getUserData(discordId)"

--- server/server.lua
+++ server/server.lua
@@ -46,58 +46,56 @@ RegisterNetEvent("playerConnecting", function(playerName, setKickReason, deferra
     if discordId ~= nil then
         discordId = discordId:sub(9)
         licenseId = licenseId:sub(9)
-        PerformHttpRequest(("https://discord.com/api/v10/guilds/%s/members/%s"):format(RIVAL.guildId, discordId), function(status, body, headers)
-            if body ~= nil then
-                local userData = json.decode(body)
-                card.body[1].text = RIVAL.locales.welcomeBack:format(userData.user.username, userData.user.discriminator)
-                card.body[2].text = RIVAL.locales.deferMessage
-                deferrals.presentCard(card)
-                for k,v in pairs(RIVAL.roles) do
-                    for i=1, #userData.roles do
-                        if userData.roles[i] == v.roleId then
-                            if RIVAL.framework == "esx" then
-                                MySQL.Async.execute("UPDATE users SET `group` = @group WHERE `identifier` = @identifier", {
-                                    group = v.groupName,
-                                    identifier = licenseId
-                                }, function()
-                                    card.body[2].text = (RIVAL.locales.authenticatedAsGroup):format(v.label)
-                                    deferrals.presentCard(card)
-                                    Wait(2000)
-                                    deferrals.done()
-                                end)
-                            elseif RIVAL.framework == "qbcore" then
-                                if assignedGroup[licenseId] then
-                                    ExecuteCommand(("remove_principal identifier.license:%s qbcore.%s"):format(licenseId, assignedGroup[licenseId]))
-                                end
-                                assignedGroup[licenseId] = v.groupName
-                                ExecuteCommand(("add_principal identifier.license:%s qbcore.%s"):format(licenseId, assignedGroup[licenseId]))
+        local inGuild, userData = getUserData(discordId)
+        if inGuild then
+            card.body[1].text = RIVAL.locales.welcomeBack:format(userData.user.username, userData.user.discriminator)
+            card.body[2].text = RIVAL.locales.deferMessage
+            deferrals.presentCard(card)
+            for k,v in pairs(RIVAL.roles) do
+                for i=1, #userData.roles do
+                    if userData.roles[i] == v.roleId then
+                        if RIVAL.framework == "esx" then
+                            MySQL.Async.execute("UPDATE users SET `group` = @group WHERE `identifier` = @identifier", {
+                                group = v.groupName,
+                                identifier = licenseId
+                            }, function()
                                 card.body[2].text = (RIVAL.locales.authenticatedAsGroup):format(v.label)
                                 deferrals.presentCard(card)
                                 Wait(2000)
                                 deferrals.done()
+                            end)
+                        elseif RIVAL.framework == "qbcore" then
+                            if assignedGroup[licenseId] then
+                                ExecuteCommand(("remove_principal identifier.license:%s qbcore.%s"):format(licenseId, assignedGroup[licenseId]))
                             end
-                            return
+                            assignedGroup[licenseId] = v.groupName
+                            ExecuteCommand(("add_principal identifier.license:%s qbcore.%s"):format(licenseId, assignedGroup[licenseId]))
+                            card.body[2].text = (RIVAL.locales.authenticatedAsGroup):format(v.label)
+                            deferrals.presentCard(card)
+                            Wait(2000)
+                            deferrals.done()
                         end
+                        return
                     end
                 end
-                if RIVAL.enforceDiscordPermissions and RIVAL.framework == "esx" then
-                    MySQL.Async.execute("UPDATE users SET `group` = @group WHERE `identifier` = @identifier", {
-                        group = "user",
-                        identifier = licenseId
-                    }, function()
-                        deferrals.done()
-                    end)
-                else
+            end
+            if RIVAL.enforceDiscordPermissions and RIVAL.framework == "esx" then
+                MySQL.Async.execute("UPDATE users SET `group` = @group WHERE `identifier` = @identifier", {
+                    group = "user",
+                    identifier = licenseId
+                }, function()
                     deferrals.done()
-                end
+                end)
             else
-                table.remove(card.body, 1)
-                card.body[1].text = RIVAL.locales.notInGuild
-                deferrals.presentCard(card)
-                Wait(2000)
                 deferrals.done()
             end
-        end, "GET", "", requestHeaders)
+        else
+            table.remove(card.body, 1)
+            card.body[1].text = RIVAL.locales.notInGuild
+            deferrals.presentCard(card)
+            Wait(2000)
+            deferrals.done()
+        end
     else
         table.remove(card.body, 1)
         card.body[1].text = RIVAL.locales.discordNotFound
@@ -131,10 +129,10 @@ CreateThread(function()
             else
                 log("Configuration valid. Have fun!")
             end
+        elseif status == 429 then
+            log("Failed to request roles from guild! This error occurs because discord rate-limited you - this happens when you have lots of applications that send too much requests to the api.")
         else
             log("Failed to request roles from guild! Check your config (guildId & botToken) and make sure the bot is on the specified guild.")
-            log("Script exiting. Configure the script and start again.")
-            os.exit()
         end
     end, "GET", "", requestHeaders)
 end)
@@ -142,3 +140,23 @@ end)
 function log(msg)
     print(("^9[^5rival_dclink^9] ^0%s"):format(msg))
 end
+
+function getUserData(discordId)
+    local isInGuild = false
+    local userData
+    PerformHttpRequest(("https://discord.com/api/v10/guilds/%s/members/%s"):format(RIVAL.guildId, discordId), function(status, body, headers)
+        if body ~= nil then
+            isInGuild = true
+            userData = json.decode(body)
+        else
+            userData = {}
+        end
+    end, "GET", "", requestHeaders)
+
+    while userData == nil do
+        Wait(0)
+    end
+    return isInGuild, userData
+end
+
+exports("getUserData", getUserData)
